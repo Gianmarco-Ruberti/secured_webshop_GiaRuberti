dd4d68dae2d404cf2d483f717efdb995446cab23
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-05-01 11:11:11 +0200

    feat(routes) : ajout du rateLimit dans la route auth
    [5][DONE]

--- app/routes/Auth.js
+++ app/routes/Auth.js
@@ -1,8 +1,9 @@
 const express    = require('express');
 const router     = express.Router();
 const controller = require('../controllers/AuthController');
+const MdpRateLimte = require('../middleware/MdpRateLimte');
 
-router.post('/login',    controller.login);
+router.post('/login',    MdpRateLimte, controller.login);
 router.post('/register', controller.register);
 
 module.exports = router;
